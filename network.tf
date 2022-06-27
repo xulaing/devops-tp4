c2aefaca72479f792f3c3baad7c04a64e9c606b9
xulaing <khauv@live.fr>
2022-06-27 17:19:38 +0200

    id_rsa

--- network.tf
+++ network.tf
@@ -1,4 +1,3 @@
-
 resource   "azurerm_public_ip"   "publicip"   { 
    name   =   "publicip-20180417" 
    location   =   var.region
@@ -20,6 +19,10 @@ resource "azurerm_network_interface" "interface" {
   }
 }
 
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                            = "devops-20180417"
@@ -27,11 +30,14 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location                        = var.region
   size                            = "Standard_D2s_v3"
   admin_username                  = "devops"
-  admin_password                  = "Password123"
-  disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.interface.id,
   ]
+
+  admin_ssh_key {
+    username   = "devops"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
 
   source_image_reference {
     publisher = "Canonical"
@@ -44,4 +50,31 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
+}
+
+resource "azurerm_network_security_group" "ubuntu-security" {
+  name                = "ubuntu-security-20180417"
+  location            = var.region
+  resource_group_name = data.azurerm_resource_group.tp4.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "ubuntu" {
+    network_interface_id      = azurerm_network_interface.interface.id
+    network_security_group_id = azurerm_network_security_group.ubuntu-security.id
 }
